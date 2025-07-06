b957fa01a164c5f66621f216ce1fcb09cffabefe
vlauncher <v2dent@gmail.com>
2025-07-06 18:27:01 +0100

    added cors

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.include_router(todo.router, prefix="/todos", tags=["Todos"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
