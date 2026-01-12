df07df9b2abff0bbba093474293938e062a6de18
vlauncher <68847935+vlauncher@users.noreply.github.com>
2026-01-12 01:12:44 +0100

    new cors

--- app/main.py
+++ app/main.py
@@ -12,6 +12,21 @@ app = FastAPI(
     version="1.0.0"
 )
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Include API router
 app.include_router(api_router)
 
